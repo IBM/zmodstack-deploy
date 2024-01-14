4852de266f0855981535baa4d3e1a3349f655a26
Midhun T <Midhun.T@ibm.com>
2024-01-14 13:58:27 +0530

    changes to execute zoscb Ansible Role via OpenShift Pipelines
    Signed-off-by: Midhun T <Midhun.T@ibm.com>

--- azure/scripts/bash/bootstrap.sh
+++ azure/scripts/bash/bootstrap.sh
@@ -165,4 +165,4 @@ ansible-playbook $GIT_CLONE_DIR/azure/scripts/ansible/playbooks/deploy.yaml \
 
 # Execute Ansible playbook to Install IBM Operator Catlog and OpenShift Pipelines
 cd $GIT_CLONE_DIR/ocp/ansible
-ansible-playbook playbooks/op-install.yaml --tags "project, pipelines"
+ansible-playbook playbooks/op-install.yaml
